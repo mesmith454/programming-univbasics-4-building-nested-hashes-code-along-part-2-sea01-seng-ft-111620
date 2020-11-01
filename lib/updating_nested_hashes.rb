0b7a7894779c1ff5fada8f764d20d10e97210e3e
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-01 19:34:59 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -26,11 +26,12 @@ def update_status
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-additional_characters = [
+
+
+epic_tragedy[:additional_characters] = additional_characters = [
   {:name => "Prince Escalus"},
   {:name => "Apothecary"}
-]
-epic_tragedy[:additional_characters] = additional_characters
+  ]
 
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
