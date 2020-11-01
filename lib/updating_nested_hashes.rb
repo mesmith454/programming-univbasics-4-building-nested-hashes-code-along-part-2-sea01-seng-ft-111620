d1a8f95a2d2d547ac0e036221bc8e0220e498bae
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-01 19:45:06 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -28,6 +28,8 @@ def update_status
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:montague][:heroine][:status]
   epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status]
+  epic_tragedy[:montague][:heroine][:status]
 end
 
 
